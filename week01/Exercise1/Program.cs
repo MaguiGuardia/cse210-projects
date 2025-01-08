17afccbe43aef8e86e1213d011be0e186534e25d
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-08 20:55:15 -0300

    added lastname

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,7 +7,11 @@ class Program
         Console.Write("What is your first name?"); 
         string name = Console.ReadLine ();
 
-        Console.Write(name);
-        
+        Console.WriteLine(name);
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine ();
+
+        Console.Write(lastname);
     }
 }
