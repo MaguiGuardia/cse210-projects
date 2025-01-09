e7140aaea73e35717a24b28c6a45216549dc73dd
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-08 21:03:44 -0300

    Happily Done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,14 +4,13 @@ class Program
 {
     static void Main(string[] args)
     { 
-        Console.Write("What is your first name?"); 
+        Console.Write("What is your first name? "); 
         string name = Console.ReadLine ();
 
-        Console.WriteLine(name);
 
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine ();
 
-        Console.Write(lastname);
+        Console.Write($"Your name is {lastname}, {name} {lastname}.");
     }
 }
