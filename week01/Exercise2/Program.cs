f80a40f7ef0f1f62e25f83f4e460f0fc41d4a079
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-09 21:58:28 -0300

    learning more

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,10 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-    
+            
         Console.WriteLine("What is your grade? ");
         string grade = Console.ReadLine();
+         int number = int.Parse(grade);
+         
+        if (grade<90)
+        {
 
-        
+        }
     }
 }
