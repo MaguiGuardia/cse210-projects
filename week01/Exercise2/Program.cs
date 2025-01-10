a007ad84b1683bdf13d005345dc5af0cbcb8e50c
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-09 22:46:14 -0300

    why doesnt work

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,17 +1,33 @@
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-            
         Console.WriteLine("What is your grade? ");
         string grade = Console.ReadLine();
-         int number = int.Parse(grade);
-         
-        if (grade<90)
-        {
+         int number = int.Parse(grade); 
 
+      string letter = "";
+
+      if (number>=90)
+        {
+            letter = "A";
         }
-    }
-}
+
+        else if (number>=80)
+        {
+            letter = "B";
+        }
+
+        else if (number>=70)
+        {
+            letter = "C";
+        }
+
+         else if (number>=60)
+         {
+            letter = "D";
+         }
+
+         else 
+         {
+            letter = "F";
+         }
+
+         Console.WriteLine("Your grade is: {letter} ");
+      
