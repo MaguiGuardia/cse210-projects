e1ffdb6e26e8b8dc74dc6e46767d9129d689e5c2
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-10 21:19:22 -0300

    adding more code

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,4 +1,5 @@
         Console.Write("what is the magic number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("Whats your guess? ");
         int userNumber = int.Parse(Console.ReadLine());
-        
 
