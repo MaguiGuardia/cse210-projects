ab0928074a86216df6c3e9840b1d26c533e5fe26
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-10 21:57:46 -0300

    second part done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,17 +1,30 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
         Console.Write("what is the magic number?");
         int magicNumber = int.Parse(Console.ReadLine());
-        Console.Write("Whats your guess? ");
-        int userNumber = int.Parse(Console.ReadLine());
-        
-        if (userNumber == magicNumber)
+
+
+        int userNumber = -1;
+
+        while (userNumber != magicNumber)
         {
+            Console.Write("Whats your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber == magicNumber)
+            {
                 Console.Write("you guessed it");
-        }
-        else if (userNumber > magicNumber)
-        {
+            }
+            else if (userNumber > magicNumber)
+            {
                 Console.Write("Lower");
-        }
-        else 
-        {
+            }
+            else
+            {
                 Console.Write("Higher");
+            }
         }
+    }
+}
