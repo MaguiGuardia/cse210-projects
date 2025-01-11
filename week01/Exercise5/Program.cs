732e2b4881e153b85f28f8b32f34384632817f06
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-11 00:39:28 -0300

    almost finish

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,24 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+        
+    }
+
+    static string PromptUserNumber(string userName)
+    {
+        Console.Write("Enter your name: ");
+    }
+
+    static int PromptUserNumber(int userNumber)
+    {
+        Console.Write("Please enter your favorite number: ");
+
+    }
+    static int DisplayResult (int userNumber)
+    {
+        int 
     }
 }
