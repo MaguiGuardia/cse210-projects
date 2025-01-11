44b814664d885afa88ab6ea4dd081edd867cb5ab
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-10 23:28:50 -0300

    added average

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -17,11 +17,14 @@ using System.Collections.Generic;
     }
 
   }
-  
+
   int sum = 0;
   foreach (int number in numbers)
   {
     sum += number;
   }
   Console.WriteLine($"The sum is: {sum}");
+ 
+ float average = ((float)sum)/ numbers.Count;
+ Console.WriteLine($"The average is: {average}");
  
