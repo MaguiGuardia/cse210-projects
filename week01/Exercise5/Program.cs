71aaca8b2e1553e0e0b07617c3d419352971c598
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-11 00:50:43 -0300

    fixing errors

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,16 +4,26 @@ class Program
 {
     static void main(string[] args)
     {
-        Console.WriteLine("Welcome to the program!");
-        
+        Console.WriteLine("Welcome to the program");
+    
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = squaredNumber (userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
     }
 
-    static string PromptUserNumber(string userName)
+    static string PromptUserName()
     {
-        Console.Write("Enter your name: ");
+        Console.Write("Please Enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
     }
 
-    static int PromptUserNumber(int userNumber)
+    static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
 
