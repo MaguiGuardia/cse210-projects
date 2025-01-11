f6b72fe549eabccb2f9df339e981373bb99cdb0b
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-10 21:29:42 -0300

    part one done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -2,4 +2,16 @@
         int magicNumber = int.Parse(Console.ReadLine());
         Console.Write("Whats your guess? ");
         int userNumber = int.Parse(Console.ReadLine());
-
+        
+        if (userNumber == magicNumber)
+        {
+                Console.Write("you guessed it");
+        }
+        else if (userNumber > magicNumber)
+        {
+                Console.Write("Lower");
+        }
+        else 
+        {
+                Console.Write("Higher");
+        }
