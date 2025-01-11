425974bb9cd2d4d1552b6ef45e4b5481bbd04eed
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-11 01:01:26 -0300

    happy ending

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,14 +2,14 @@ using System;
 
 class Program
 {
-    static void main(string[] args)
+    static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the program");
     
         string userName = PromptUserName();
         int userNumber = PromptUserNumber();
 
-        int squaredNumber = squaredNumber (userNumber);
+        int squaredNumber = SquaredNumber(userNumber);
 
         DisplayResult(userName, squaredNumber);
 
@@ -26,10 +26,20 @@ class Program
     static int PromptUserNumber()
     {
         Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
 
     }
-    static int DisplayResult (int userNumber)
+
+    static int SquaredNumber(int number)
     {
-        int 
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, te square of your number is {square}");
     }
 }
