7b08e8e69753d5522716de5b5963bc18ddacb6ca
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-17 01:32:16 -0300

    working on class

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+       
+        while(true)
+        {
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. save");
+            Console.WriteLine("4. load");
+            Console.WriteLine("5. quit");
+
+            Console.Write("enter selection: ");
+            string userImput=Console.ReadLine();
+
+            Console.WriteLine($"user selected {userImput}");
+
+            if (userImput == "1")
+            {
+                Console.WriteLine("Write New Entry");
+
+            }
+            else if (userImput == "2")
+            {
+                Console.WriteLine("Display Journal");
+            }
+            else if (userImput == "3")
+            {
+                Console.WriteLine("Save Journal");
+            }
+            else if (userImput == "4")
+            {
+             
+                Console.WriteLine("Write New Entry");
+            }
+            else if (userImput == "5")
+            {
+                
+            }
+            else 
+            {
+                Console.WriteLine("Please try again!");
+            }
+        }
+        
     }
 }
