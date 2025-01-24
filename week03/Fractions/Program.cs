28fcc275a97650ffd93b57fd56c04796fe359698
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-24 20:24:19 -0300

    added getters and setters and constructors

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -8,13 +8,18 @@ class Program
         Console.WriteLine(F1.GetFractionString());
         Console.WriteLine(F1.GetDecimalValue());
 
-        Fractions F2 = new(6);
+        Fractions F2 = new(5);
         Console.WriteLine(F2.GetFractionString());
         Console.WriteLine(F2.GetDecimalValue());
 
-        Fractions F3 = new(6, 7);
+        Fractions F3 = new(3, 4);
         Console.WriteLine(F3.GetFractionString());
         Console.WriteLine(F3.GetDecimalValue());
+
+        Fractions F4 = new(1, 3);
+        Console.WriteLine(F4.GetFractionString());
+        Console.WriteLine(F4.GetDecimalValue());
+        
     }
 
 
