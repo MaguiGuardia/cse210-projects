4fcc9427453c82731cbe111828196616faa96711
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-25 03:17:25 -0300

    added a Console clear and other fixes

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -9,6 +9,12 @@ class Program
 
         Scripture scr1 = new("I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them");
         Console.WriteLine(scr1.GetDisplayText());
+
+
+        Console.WriteLine("Press enter to continue or type 'quit' to finish");
+        Console.ReadLine();
+
+        Console.Clear();
         
     }
 }
