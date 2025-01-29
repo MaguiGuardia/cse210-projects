79ffe52d65c049db88a056fa8816dc7fcb8aed01
magaliguardia <magaliquimeyguardia@gmail.com>
2025-01-29 00:39:11 -0300

    starting with classes

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,16 @@
+
+public class Comment()
+{
+    private string _userName;
+    private string _textComment;
+
+    public string GetUserName()
+    {
+        return _userName;
+    }
+
+    public string GetTextComment()
+    {
+        return _textComment;
+    }
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,25 @@
+
+using System.ComponentModel;
+using System.Transactions;
+
+public class Video()
+{
+    private string _title;
+    private string _author;
+    private int _length;
+
+    private List<Comment> _comment = new List<Comment>();
+
+    public Video(string List<Comment>, string _title , string _author , int _length)
+    {
+        return;
+    }
+
+    public string DisplayVideoInfo()
+    {
+        return  _author;
+        return _title;
+        return _length;
+        
+    }
+}
